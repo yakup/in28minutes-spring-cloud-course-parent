33a94d33eba0483c591386e18b629b9ef02af540
yakup <yakup@ode.al>
2021-04-07 17:35:36 +0300

    Step 18 - Setting up Currency Conversion Microservice

---
+++ currency-conversion-service/src/main/java/com/in28minutes/course/master/spring/cloud/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.course.master.spring.cloud.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConversionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+    }
+
+}
