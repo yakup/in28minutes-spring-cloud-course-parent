e24b1be7593771586f6d271ee3add9841355beb7
yakup <yakup@ode.al>
2021-04-08 10:41:24 +0300

    Step 27 - Connecting Currency Exchange microservice to Eureka

--- currency-exchanger/src/main/java/com/in28minutes/course/masterspringcloud/currencyexchanger/CurrencyExchangerApplication.java
+++ currency-exchanger/src/main/java/com/in28minutes/course/masterspringcloud/currencyexchanger/CurrencyExchangerApplication.java
@@ -2,8 +2,10 @@ package com.in28minutes.course.masterspringcloud.currencyexchanger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CurrencyExchangerApplication {
 
 	public static void main(String[] args) {
