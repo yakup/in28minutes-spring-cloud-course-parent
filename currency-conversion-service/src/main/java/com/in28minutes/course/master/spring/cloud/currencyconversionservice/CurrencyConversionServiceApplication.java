ebb73226f48c6b9141996c721eff166ec0bac53d
yakup <yakup@ode.al>
2021-04-12 17:18:44 +0300

    Set Ribbon to Eureka discovery

--- currency-conversion-service/src/main/java/com/in28minutes/course/master/spring/cloud/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/in28minutes/course/master/spring/cloud/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -1,15 +1,12 @@
 package com.in28minutes.course.master.spring.cloud.currencyconversionservice;
 
-import com.in28minutes.course.master.spring.cloud.currencyconversionservice.configuration.LocalRibbonClientConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.in28minutes.course.master.spring.cloud.currencyconversionservice")
-@RibbonClient(name = "currency-conversion-service", configuration = LocalRibbonClientConfiguration.class)
 @EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
